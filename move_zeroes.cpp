55e47663bb4132f4fc3e8cc412f90047fde35ce5
Harpreet Kour <100257594+HKour2003@users.noreply.github.com>
2024-07-22 13:24:25 +0530

    Update move_zeroes.cpp

--- move_zeroes.cpp
+++ move_zeroes.cpp
@@ -1,5 +1,22 @@
 class Solution {
 public:
+    void moveZeroes(vector<int>& nums) {
+    
+        vector<int>numbers;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=0) numbers.push_back(i);
+        }
+        for(int i=0;i<numbers.size();i++){
+            nums[i]=nums[numbers[i]];
+        }
+        for(int i=numbers.size();i<nums.size();i++){
+            nums[i]=0;
+        }
+    }
+};
+// second approach
+class Solution {
+public:
    void moveZeroes(vector<int>& nums) {
    int size=nums.size();
    for(auto i=nums.begin();i<nums.end();){
