358d3237a1de171d04b4f3aecaa58b3a0a09d072
Harpreet Kour <100257594+HKour2003@users.noreply.github.com>
2024-08-07 15:47:56 +0530

    Update Longest Valid Parentheses.cpp

--- Longest Valid Parentheses.cpp
+++ Longest Valid Parentheses.cpp
@@ -1,4 +1,5 @@
-class Solution {
+/* approach 1
+    class Solution {
 public:
     int longestValidParentheses(string s) {
       stack<int> st;
@@ -22,3 +23,51 @@ public:
     return maxl;
     }
 };
+*/
+// approach 2
+class Solution {
+public:
+
+    int longestValidParentheses(string s) {
+        
+        int count1=0;
+        int count2=0;
+        int maxl=0;
+        int max2=0;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='('){
+                count1++;
+            }
+            else if(s[i]==')'){
+                count2++;
+            }
+            if(count1==count2){
+                maxl=max(maxl,count1+count2);
+            }
+            else if(count1 < count2){
+                count1=0;
+                count2=0;
+            }
+            }
+count1=count2=0;            
+            for(int i=s.length()-1;i>=0;i--){
+            if(s[i]=='('){
+                count1++;
+            }
+            else if(s[i]==')'){
+                count2++;
+            }
+            if(count1==count2){
+                max2=max(max2,count1+count2);
+            }
+            else if(count1 > count2){
+                count1=0;
+                count2=0;
+            }
+            }
+            int res= max(maxl,max2);
+            return res;
+            }
+    };
+
+    
