3198829cab51760ce018e9a9c4b8ff087075abe6
Harpreet Kour <100257594+HKour2003@users.noreply.github.com>
2024-08-09 15:44:23 +0530

    Update Search in a Binary Search Tree.cpp

--- Search in a Binary Search Tree.cpp
+++ Search in a Binary Search Tree.cpp
@@ -13,7 +13,9 @@ class Solution {
 public:
    
     TreeNode* searchBST(TreeNode* root, int val) {
-         if(root==NULL||root->val==val){
+         if(root==NULL){
+            return NULL;}
+            if(root->val==val){
           return root;
          } 
         if(root->val<val){
