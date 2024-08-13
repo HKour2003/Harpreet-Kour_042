87bbda22b35610ccf44ccfb896fa1e85c5fa8113
Harpreet Kour <100257594+HKour2003@users.noreply.github.com>
2024-08-13 12:08:32 +0530

    Update Kth Smallest Element in a BST.cpp

--- Kth Smallest Element in a BST.cpp
+++ Kth Smallest Element in a BST.cpp
@@ -8,7 +8,7 @@
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
- */
+ 
 class Solution {
 public:
     void inorder(TreeNode* root,vector<int>&v)
@@ -23,5 +23,30 @@ public:
         vector<int>vc;
         inorder(root,vc);
         return vc[k - 1];
+    }
+};
+*/
+//2nd approach
+class Solution {
+public:
+    int count=0;
+    int r=0;
+
+
+    void inorder(TreeNode* root, int k) {
+        if (root!=NULL){
+        inorder(root->left,k);
+        count++;
+        if(count==k){
+            r=root->val;
+            return;
+        }
+        inorder(root->right,k);
+        }}
+        
+        
+    int kthSmallest(TreeNode* root, int k) {
+        inorder(root,k);
+        return r;
     }
 };
